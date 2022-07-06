5d372752390f5971e150f2ca8f9b4fc8420fead7
NachoNievaG <jose.ign.nieva@gmail.com>
2022-07-06 15:20:03 -0300

    feat | colorscheme & cmp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,22 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }) -- Markdown visualizer
 
+  -- Colorscheme
+  use({
+    "catppuccin/nvim",
+    as = "catppuccin"
+  }) 
+
+  -- CMP
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
