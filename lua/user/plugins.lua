14a1285a79ca6d8660bebb459f959b9037e01e81
NachoNievaG <jose.ign.nieva@gmail.com>
2022-10-18 10:50:14 -0300

    fix(cmp): deprecated function

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -190,7 +190,16 @@ return packer.startup(function(use)
     require("transparent").setup({
       enable = true, -- boolean: enable transparent
       extra_groups = { -- table/string: additional groups that should be cleared
-        { "all" }
+        {
+          "all",
+          "nvim-tree",
+          "BufferLineTabClose",
+          "BufferlineBufferSelected",
+          "BufferLineFill",
+          "BufferLineBackground",
+          "BufferLineSeparator",
+          "BufferLineIndicatorSelected",
+        }
       },
       exclude = {}, -- table: groups you don't want to clear
     })
